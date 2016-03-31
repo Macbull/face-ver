d00304e5b98621d543dcb38fb3218664869b6ea5
Vishwajeet Narwal <vnarwal95@gmail.com>
2016-03-31 07:42:10 +0530

    client.py

--- src/face_ver/client.py
+++ src/face_ver/client.py
@@ -34,19 +34,21 @@ import threading
 from grpc.beta import implementations
 import numpy
 import tensorflow as tf
-
+from scipy import ndimage
+import numpy as np
 from tensorflow_serving.example import inference_pb2
 
-
-tf.app.flags.DEFINE_integer('concurrency', 1,
-                            'maximum number of concurrent inference requests')
-tf.app.flags.DEFINE_integer('num_tests', 100, 'Number of test images')
+tf.app.flags.DEFINE_string('image1_file', 'test/img1.pgm', 'Input image 1')
+tf.app.flags.DEFINE_string('image2_file', 'test/img1.pgm', 'Input image 2')
+tf.app.flags.DEFINE_integer('pixel_depth', 255,
+                            'maximum intensity of pixel')
 tf.app.flags.DEFINE_string('server', '', 'mnist_inference service host:port')
-tf.app.flags.DEFINE_string('work_dir', '/tmp', 'Working directory. ')
+
 FLAGS = tf.app.flags.FLAGS
 
+image_size = [112,92]
 
-def do_inference(hostport, work_dir, concurrency, num_tests):
+def do_inference(hostport, input):
   """Tests mnist_inference service with concurrent requests.
 
   Args:
@@ -61,44 +63,24 @@ def do_inference(hostport, work_dir, concurrency, num_tests):
   Raises:
     IOError: An error occurred processing test data set.
   """
-  test_data_set = mnist_input_data.read_data_sets(work_dir).test
   host, port = hostport.split(':')
   channel = implementations.insecure_channel(host, int(port))
   stub = mnist_inference_pb2.beta_create_MnistService_stub(channel)
-  cv = threading.Condition()
   result = {'active': 0, 'error': 0, 'done': 0}
-  def done(result_future, label):
-    with cv:
-      exception = result_future.exception()
-      if exception:
-        result['error'] += 1
-        print exception
-      else:
-        sys.stdout.write('.')
-        sys.stdout.flush()
-        response = numpy.array(result_future.result().value)
-        prediction = numpy.argmax(response)
-        if label != prediction:
-          result['error'] += 1
-      result['done'] += 1
-      result['active'] -= 1
-      cv.notify()
-  for _ in range(num_tests):
-    request = mnist_inference_pb2.MnistRequest()
-    image, label = test_data_set.next_batch(1)
-    for pixel in image[0]:
-      request.image_data.append(pixel.item())
-    with cv:
-      while result['active'] == concurrency:
-        cv.wait()
-      result['active'] += 1
-    result_future = stub.Classify.future(request, 5.0)  # 5 seconds
-    result_future.add_done_callback(
-        lambda result_future, l=label[0]: done(result_future, l))  # pylint: disable=cell-var-from-loop
-  with cv:
-    while result['done'] != num_tests:
-      cv.wait()
-    return result['error'] / float(num_tests)
+  request = inference_pb2.VerifyRequest()
+  for pixel in data.flatten():
+    request.image_data.append(pixel.item())
+  result_future = stub.Classify(request, 5.0)
+  exception = result_future.exception()
+  if exception:
+    print exception
+  else:
+    sys.stdout.write('.')
+    sys.stdout.flush()
+    response = numpy.array(result_future.message)
+    print(response)
+
+
 
 
 def main(_):
@@ -108,9 +90,18 @@ def main(_):
   if not FLAGS.server:
     print 'please specify server host:port'
     return
-  error_rate = do_inference(FLAGS.server, FLAGS.work_dir,
+
+  image1 = (ndimage.imread(image1_file).astype(float)*2 - pixel_depth) /pixel_depth
+  image2 = (ndimage.imread(image2_file).astype(float)*2 - pixel_depth) /pixel_depth
+  data = np.ndarray(shape = (2, 1, image_size[0], image_size[1],1), dtype=np.float32)
+  data[0,0,:,:,0] = image1
+  data[1,0,:,:,0] = image2
+
+  result = do_inference(FLAGS.server, data,
                             FLAGS.concurrency, FLAGS.num_tests)
-  print '\nInference error rate: %s%%' % (error_rate * 100)
+
+
+  
 
 
 if __name__ == '__main__':
